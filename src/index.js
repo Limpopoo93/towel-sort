96fc30a620ea2825fe0824bf11470055ab314b14
sigchi-web <sigchi.dev@gmail.com>
2023-01-17 19:33:44 +0300

    init: towel-sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,59 @@
-
 // You should implement your task here.
+module.exports = function towelSort(matrix) {
+    if (matrix.length < 1) {
+        return [];
+    } else {
+        let result = [];
+        if (Array.isArray(matrix[0])) {
+            for (let i = 0; i < matrix.length; i++) {
+                if (i % 2 === 0) {
+                    for (let w = 0; w < matrix[i].length; w++) {
+                        result.push(matrix[i][w]);
+                    }
+                } else {
+                    for (let w = matrix[i].length - 1; w > -1; w--) {
+                        result.push(matrix[i][w]);
+                    }
+                }
+            }
+        } else {
+            for (let i = 0; i < matrix.length; i++) {
+                result.push(matrix[i]);
+            }
+        }
 
-module.exports = function towelSort (matrix) {
-  return [];
+        return result;
+    }
 }
+
+
+/*
+function sort(matrix) {
+    let result = [];
+
+    for (let i = 0; i < matrix.length; i++) {
+        console.log(Array.isArray(matrix[i]));
+        if (i % 2 === 0) {
+            for (let w = 0; w < matrix[i].length; w++) {
+                result.push(matrix[i][w]);
+                /!*= result + matrix[i][w] + ",";*!/
+            }
+        } else {
+            for (let w = matrix[i].length - 1; w > -1; w--) {
+                result.push(matrix[i][w]);
+                /!*result = result + matrix[i][w] + ",";*!/
+            }
+        }
+    }
+    console.log(result);
+}
+
+
+sort([
+    1, 2, 3
+]);
+
+
+*/
+
+
